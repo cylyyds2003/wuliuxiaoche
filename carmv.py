5e8a56668c904cd0d0da89427c9c5bcb2cc6ac9f
cylyyds2003 <1352951127@qq.com>
2024-11-29 21:22:30 +0800

    测试

--- carmv.py
+++ carmv.py
@@ -1,20 +1,22 @@
-#实验名称：2024物流机器人视觉代码
+﻿#实验名称�?2024物流机器人视觉代�?
 #版本：v1.0
-#日期：2024.10
-#翻译和注释：张恩璐
+#日期�?2024.10
+#翻译和注释：张恩�?
 import sensor, image, time,pyb
 from pyb import UART
 
-#状态机 0为寻找二维码 1为物块识别 2为靶心识别
-flag = 2
-#颜色标志位 0为红色 1为绿色 2为蓝色
-color_flag = 0
-#颜色识别阈值
-thresholds = [(21, 39, 10, 62, 8, 127), # 红色阈值
-              (35, 75, -128, -4, -128, 127), # 绿色阈值
-              (17, 39, -128, 127, -128, 0)] # 蓝色阈值
+#测试
 
-#寻找最大色块函数定义
+#状态机 0为寻找二维码 1为物块识�? 2为靶心识�?
+flag = 2
+#颜色标志�? 0为红�? 1为绿�? 2为蓝�?
+color_flag = 0
+#颜色识别阈�?
+thresholds = [(21, 39, 10, 62, 8, 127), # 红色阈�?
+              (35, 75, -128, -4, -128, 127), # 绿色阈�?
+              (17, 39, -128, 127, -128, 0)] # 蓝色阈�?
+
+#寻找最大色块函数定�?
 def find_max(blobs):
     max_size=0
     for blob in blobs:
@@ -22,7 +24,7 @@ def find_max(blobs):
             max_blob=blob
             max_size = blob[2]*blob[3]
     return max_blob
-#寻找最小半径的圆函数定义
+#寻找最小半径的圆函数定�?
 def find_min_circle(circles):
     min_radius = circles[0].r()
     min_circle = circles[0]
@@ -37,7 +39,7 @@ sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QQVGA)
 sensor.skip_frames(time = 2000)
 
-uart = UART(3,115200)#设置串口波特率
+uart = UART(3,115200)#设置串口波特�?
 
 clock = time.clock()
 
@@ -71,7 +73,7 @@ while(True):
 
 
     if flag == 0:
-        res = img.find_qrcodes() #寻找二维码
+        res = img.find_qrcodes() #寻找二维�?
         if len(res) > 0:
             img.draw_rectangle(res[0].rect())
             QRcode = res[0].payload()
@@ -80,55 +82,55 @@ while(True):
             print(res[0].payload())
     elif flag == 1:
         if color_flag == 0:
-            blobs = img.find_blobs([thresholds[0]],merge=True) # 0,1,2分别表示红，绿，蓝色。
+            blobs = img.find_blobs([thresholds[0]],merge=True) # 0,1,2分别表示红，绿，蓝色�?
             if blobs:
                 max_blob = find_max(blobs)
                 txbuff = bytearray([0xA6,int(max_blob.cx()),int(max_blob.cy()),0xFF])
                 uart.write(txbuff)
-                img.draw_rectangle((max_blob.x(),max_blob.y(),max_blob.w(),max_blob.h()),color=(255,0,0))#用红色框出最大色块
+                img.draw_rectangle((max_blob.x(),max_blob.y(),max_blob.w(),max_blob.h()),color=(255,0,0))#用红色框出最大色�?
         elif color_flag == 1:
-            blobs = img.find_blobs([thresholds[1]],merge=True) # 0,1,2分别表示红，绿，蓝色。
+            blobs = img.find_blobs([thresholds[1]],merge=True) # 0,1,2分别表示红，绿，蓝色�?
             if blobs:
                 max_blob = find_max(blobs)
                 txbuff = bytearray([0xA6,int(max_blob.cx()),int(max_blob.cy()),0xFF])
                 uart.write(txbuff)
-                img.draw_rectangle((max_blob.x(),max_blob.y(),max_blob.w(),max_blob.h()),color=(255,0,0))#用红色框出最大色块
+                img.draw_rectangle((max_blob.x(),max_blob.y(),max_blob.w(),max_blob.h()),color=(255,0,0))#用红色框出最大色�?
         elif color_flag == 2:
-            blobs = img.find_blobs([thresholds[2]],merge=True) # 0,1,2分别表示红，绿，蓝色。
+            blobs = img.find_blobs([thresholds[2]],merge=True) # 0,1,2分别表示红，绿，蓝色�?
             if blobs:
                 max_blob = find_max(blobs)
                 txbuff = bytearray([0xA6,int(max_blob.cx()),int(max_blob.cy()),0xFF])
                 uart.write(txbuff)
-                img.draw_rectangle((max_blob.x(),max_blob.y(),max_blob.w(),max_blob.h()),color=(255,0,0))#用红色框出最大色块
+                img.draw_rectangle((max_blob.x(),max_blob.y(),max_blob.w(),max_blob.h()),color=(255,0,0))#用红色框出最大色�?
     elif flag == 2:
         if color_flag == 0:
-            blobs = img.find_blobs([thresholds[0]],merge=True) # 0,1,2分别表示红，绿，蓝色。
+            blobs = img.find_blobs([thresholds[0]],merge=True) # 0,1,2分别表示红，绿，蓝色�?
             if blobs:
                 circles = img.find_circles(threshold = 5000, x_margin = 10, y_margin = 10, r_margin = 10,
                                                r_min = 20, r_max = 40, r_step = 2)
                 if circles:
                     min_circle=find_min_circle(circles)
-                    # 可以在这里对最小半径的圆min_circle进行其他操作，比如绘制
+                    # 可以在这里对最小半径的圆min_circle进行其他操作，比如绘�?
                     img.draw_circle(min_circle.x(), min_circle.y(), min_circle.r(), color=(255, 0, 0))
                     txbuff = bytearray([0xA7,int(min_circle.x()),int(min_circle.y()),0xFF])
         elif color_flag == 1:
-            blobs = img.find_blobs([thresholds[1]],merge=True) # 0,1,2分别表示红，绿，蓝色。
+            blobs = img.find_blobs([thresholds[1]],merge=True) # 0,1,2分别表示红，绿，蓝色�?
             if blods:
                 circles = img.find_circles(threshold = 5000, x_margin = 10, y_margin = 10, r_margin = 10,
                                                r_min = 20, r_max = 40, r_step = 2)
                 if circles:
                     min_circle=find_min_circle(circles)
-                    # 可以在这里对最小半径的圆min_circle进行其他操作，比如绘制
+                    # 可以在这里对最小半径的圆min_circle进行其他操作，比如绘�?
                     img.draw_circle(min_circle.x(), min_circle.y(), min_circle.r(), color=(255, 0, 0))
                     txbuff = bytearray([0xA7,int(min_circle.x()),int(min_circle.y()),0xFF])
         elif color_flag == 2:
-            blobs = img.find_blobs([thresholds[2]],merge=True) # 0,1,2分别表示红，绿，蓝色。
+            blobs = img.find_blobs([thresholds[2]],merge=True) # 0,1,2分别表示红，绿，蓝色�?
             if blobs:
                 circles = img.find_circles(threshold = 5000, x_margin = 10, y_margin = 10, r_margin = 10,
                                                r_min = 20, r_max = 40, r_step = 2)
                 if circles:
                     min_circle=find_min_circle(circles)
-                    # 可以在这里对最小半径的圆min_circle进行其他操作，比如绘制
+                    # 可以在这里对最小半径的圆min_circle进行其他操作，比如绘�?
                     img.draw_circle(min_circle.x(), min_circle.y(), min_circle.r(), color=(255, 0, 0))
                     txbuff = bytearray([0xA7,int(min_circle.x()),int(min_circle.y()),0xFF])
     #print(clock.fps())
